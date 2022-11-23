e28fd73107c67a2b219deb11735153da42cfc444
Alan <afigueroa33@ucol.mx>
2022-11-23 14:26:31 -0600

    Fly.io

--- src/app.js
+++ src/app.js
@@ -1,63 +1,40 @@
 import express from "express";
-
 import { createPool } from "mysql2/promise";
 
- 
-
 const pool=createPool({
-
     user:'root',
-
     password:'AFNc3hgap2SkO76yHwcW',
-
     host:'containers-us-west-77.railway.app',
-
     port:6311,
-
     database:'railway'
-
 })
 
-const app=express()
-
- 
+const app=express() 
 
 app.get('/',(req,res)=>{
-
   res.send("Bienvenido a este servidor..")      
-
 })
 
- 
-
 app.get('/usuarios',async (req,res)=>{
-
     const [result]=await pool.query('select * from usuario')
-
     res.json(result)      
+}) 
 
-  })
-
- 
-
-  app.get('/agregarusuario',async (req,res)=>{
-
+app.get('/agregarusuario',async (req,res)=>{
     const nombre=req.query.nombre
-
     const contrasena=req.query.contrasena
-
     const correo=req.query.correo
-
     const tienda=req.query.tienda  
-
     const [result]=await pool.query(`INSERT INTO usuario (nombre, contrasena, correo, tienda) VALUES ('${nombre}', '${contrasena}', '${correo}','${tienda}')`)
-
     res.json(result[0])      
-
   })
 
- 
+app.get('/login', async (req,res)=>{
+  const correo=req.query.correo
+  const contrasena=req.query.contrasena
+  const [result]=await pool.query(`select * from usuario where correo='${correo}' and contrasena='${contrasena}'`)
+  res.json(result[0])
+})
 
 app.listen(process.env.PORT || 3000)
-
 console.log("Servidor corriendo en el puerto 3000")
