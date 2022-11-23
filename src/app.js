877a105982eb156f131c35244b792d09d4db2521
Alan <72175280+alan-figueroa-iglesias@users.noreply.github.com>
2022-11-23 14:30:58 -0600

    Update app.js

--- src/app.js
+++ src/app.js
@@ -33,7 +33,7 @@ app.get('/login', async (req,res)=>{
   const correo=req.query.correo
   const contrasena=req.query.contrasena
   const [result]=await pool.query(`select * from usuario where correo='${correo}' and contrasena='${contrasena}'`)
-  res.json(result[0])
+  console.log("Ok")
 })
 
 app.listen(process.env.PORT || 3000)
